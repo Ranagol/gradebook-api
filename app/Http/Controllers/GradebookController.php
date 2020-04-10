e40d9b868981d4889117a2c55a9ccf28cd46c002
Andor <andorhorvat@gmail.com>
2020-04-11 00:09:18 +0200

    Models, controllers, migrations

---
+++ app/Http/Controllers/GradebookController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Gradebook;
+use Illuminate\Http\Request;
+
+class GradebookController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Gradebook  $gradebook
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Gradebook $gradebook)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Gradebook  $gradebook
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Gradebook $gradebook)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Gradebook  $gradebook
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Gradebook $gradebook)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Gradebook  $gradebook
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Gradebook $gradebook)
+    {
+        //
+    }
+}
