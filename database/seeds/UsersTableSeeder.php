8e3d48e2c0174f07f56fdf1173d5ebfd46caf6cc
Andor <andorhorvat@gmail.com>
2020-04-17 15:47:16 +0200

    UserSeeder correction

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -12,11 +12,12 @@ class UsersTableSeeder extends Seeder
     public function run()
     {
         $user = factory(App\User::class, 35)->create()
-            ->each(function(App\User $user) {
-                $user->professor()->save([
-                    'first_name' => $user->first_name,
-                    'last_name' => $user->last_name
-                ]);
+            ->each(function(App\User $user) {//TODO LOSI EZ ITT NEM JO, NEM LEHET SEEDELNI
+                $professor = new App\Professor();
+                $professor->first_name = $user->first_name;
+                $professor->last_name = $user->last_name;
+
+                $user->professor()->save($professor);
             });
     }
 }
